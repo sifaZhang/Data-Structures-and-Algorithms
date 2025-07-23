d808b7e3333a4a3fae8bf749da5cd0a1bd4cfd7f
Xiaona Jia <xiaona.jia@localcover.com>
2025-07-23 18:54:04 +1200

    disable warning

--- Program.cs
+++ Program.cs
@@ -3,7 +3,9 @@ using System.Net.Http.Headers;
 
 Console.WriteLine("Please input the number:");
 Console.WriteLine("01: Random Array");
+#pragma warning disable CS8600 // Converting null literal or possible null value to non-nullable type.
 string choice = Console.ReadLine();
+#pragma warning restore CS8600 // Converting null literal or possible null value to non-nullable type.
 
 switch (choice)
 {
