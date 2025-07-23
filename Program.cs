2ad573270fe924ea22d5ce90befd95c1165c1c18
Xiaona Jia <xiaona.jia@localcover.com>
2025-07-23 13:36:24 +1200

    first commit

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Net.Http.Headers;
+
+Console.WriteLine("Please input the number:");
+Console.WriteLine("01: Random Array");
+string choice = Console.ReadLine();
+
+switch (choice)
+{
+  case "01":
+    practice01.Run();
+    break;
+  default:
+    Console.WriteLine("Invalid input!");
+    break;
+}
---
+++ practice01_RandomArray.cs
@@ -0,0 +1,40 @@
+
+public class practice01
+{
+  public static void Run()
+  {
+    Console.WriteLine("----------------01 start----------------");
+
+    int iLen = 5;
+    var rand = new Random();
+    int[] scores = { rand.Next(100), rand.Next(100), rand.Next(100), rand.Next(100), rand.Next(100) };
+    Console.Write("Generate random array:\n");
+    for (int i = 0; i < iLen; i++)
+    {
+      Console.WriteLine($"{i}:{scores[i]}");
+    }
+
+
+    int iTemp = 0;
+    for (int i = iLen - 1; i > 0; i--)
+    {
+      for (int j = 0; j < i; j++)
+      {
+        if (scores[j] > scores[j + 1])
+        {
+          iTemp = scores[j + 1];
+          scores[j + 1] = scores[j];
+          scores[j] = iTemp;
+        }
+      }
+    }
+
+    Console.Write("After sorted:\n");
+    for (int i = 0; i < iLen; i++)
+    {
+      Console.WriteLine($"{i}:{scores[i]}");
+    }
+
+     Console.WriteLine("----------------01 end----------------");
+  }
+}
