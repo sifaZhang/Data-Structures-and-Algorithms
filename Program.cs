3f45a9d75a1023606ad28d87ebac288e9b88b4ce
sifaZhang <sifazhang.nz@gmail.com>
2025-07-30 08:43:05 +1200

    add practice 2

--- Program.cs
+++ Program.cs
@@ -1,18 +1,23 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using HelloWorld;
 using System.Net.Http.Headers;
 
 Console.WriteLine("Please input the number:");
 Console.WriteLine("01: Random Array");
+Console.WriteLine("02: Peak in an Array");
 #pragma warning disable CS8600 // Converting null literal or possible null value to non-nullable type.
 string choice = Console.ReadLine();
 #pragma warning restore CS8600 // Converting null literal or possible null value to non-nullable type.
 
 switch (choice)
 {
-  case "01":
-    practice01.Run();
-    break;
-  default:
-    Console.WriteLine("Invalid input!");
-    break;
+    case "01":
+        practice01.Run();
+        break;
+    case "02":
+        practice02_FindPeakInArray.Run();
+        break;
+    default:
+        Console.WriteLine("Invalid input!");
+        break;
 }
---
+++ practice02_FindPeakInArray.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace HelloWorld
+{
+    internal class practice02_FindPeakInArray
+    {
+        public static void Run()
+        {
+            uint count = 20;
+            int[] numbers = new int[count];
+
+            for (int i = 0; i < count; i++)
+            {
+                numbers[i] = new Random().Next(1000);
+            }
+
+            if (count > 0)
+            {
+                int iMin = numbers[0];
+                int iMax = numbers[0];
+                for (int i = 0; i < count; i++)
+                {
+                    Console.WriteLine($"Number[{i}]: {numbers[i]}");
+                    if (numbers[i] > iMax)
+                    {
+                        iMax = numbers[i];
+                    }
+                    else if (numbers[i] < iMin)
+                    {
+                        iMin = numbers[i];
+                    }
+                    else
+                    {
+
+                    }
+                }
+
+                Console.WriteLine($"The max number: {iMax}");
+                Console.WriteLine($"The min number:{iMin}");
+            }
+        }
+    }
+}
