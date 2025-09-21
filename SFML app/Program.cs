13d6452add09e0bb837dbca9a544c9645e333467
sifaZhang <sifazhang.nz@gmail.com>
2025-09-21 17:13:14 +1200

    add arrow and modify the structure of the code

--- SFML app/Program.cs
+++ SFML app/Program.cs
@@ -5,6 +5,7 @@ using SFML_app;
 using System;
 using System.Text.RegularExpressions;
 using static SFML.Window.Mouse;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace RotatingHelloWorldSfmlDotNetCoreCSharp
 {
@@ -14,117 +15,258 @@ namespace RotatingHelloWorldSfmlDotNetCoreCSharp
         {
             Console.WriteLine("Press ESC key to close window");
             MyWindow window = new MyWindow();
-            window.Show();
+            window.Run();
             Console.WriteLine("All done");
         }
     }
 
     class MyWindow
     {
-        public void Show()
+        private RenderWindow renderWindow;
+        private Font font;
+        private MyTextBox txtInput;
+        private MyLabel errInput;
+        private MyButton btnEnqueue;
+        private MyButton btnDequeue;
+        private MyButton btnFetch;
+        private MyButton btnClear;
+        private QueueVisualizer queue;
+        private bool mouseHeld;
+        private bool mouseReleased;
+        private Color defaultColor = new Color(200, 230, 255);
+        private ConvexShape arrow;
+        private Text headLabel;
+        private bool roate = true;
+        private Clock clock;
+        private float delta, angle, angleSpeed;
+
+        public MyWindow()
         {
             VideoMode mode = new VideoMode(1980, 900);
-            RenderWindow window = new RenderWindow(mode, "SFML.NET");
-            Font font = new Font("C:/Windows/Fonts/arial.ttf");
+            renderWindow = new RenderWindow(mode, "SFML.NET");
 
-            MyTextBox txtInput = new MyTextBox(new Vector2f(50, 100), new Vector2f(200, 50), font);
-            MyLabel errInput = new MyLabel(new Vector2f(300, 100), new Vector2f(400, 50), font);
+            renderWindow.Closed += Window_Closed;
+            renderWindow.KeyPressed += Window_KeyPressed;
+            renderWindow.KeyReleased += Window_KeyReleased;
 
-            MyButton btnEnqueue = new MyButton(new Vector2f(50, 700), new Vector2f(200, 100), "Enquene", font);
-            MyButton btnDequeue = new MyButton(new Vector2f(300, 700), new Vector2f(200, 100), "Dequene", font);
-            MyButton btnFetch = new MyButton(new Vector2f(550, 700), new Vector2f(200, 100), "Fetch", font);
-            MyButton btnClear = new MyButton(new Vector2f(800, 700), new Vector2f(200, 100), "Clear", font);
+            font = new Font("C:/Windows/Fonts/arial.ttf");
 
-            QueueVisualizer queue = new QueueVisualizer(font, new Vector2f(50, 300));
+            txtInput = new MyTextBox(new Vector2f(50, 100), new Vector2f(200, 50), font);
+            errInput = new MyLabel(new Vector2f(300, 100), new Vector2f(400, 50), font);
 
-            bool mouseHeld = false;
-            bool mouseReleased = false;
+            btnEnqueue = new MyButton(new Vector2f(50, 700), new Vector2f(200, 100), "Enquene", font);
+            btnDequeue = new MyButton(new Vector2f(300, 700), new Vector2f(200, 100), "Dequene", font);
+            btnFetch = new MyButton(new Vector2f(550, 700), new Vector2f(200, 100), "Fetch", font);
+            btnClear = new MyButton(new Vector2f(800, 700), new Vector2f(200, 100), "Clear", font);
 
-            window.Closed += (obj, e) => { window.Close(); };
-            window.MouseButtonPressed += (_, e) => {
+            queue = new QueueVisualizer(font, new Vector2f(50, 260));
+            queue.SetColor(defaultColor);
+
+            arrow = new ConvexShape(3);
+            headLabel = new Text("Head", font, 28)
+            {
+                FillColor = Color.Red,
+                Position = new Vector2f(-1, -1)
+            };
+
+            clock = new Clock();
+            delta = 0;
+            angle = 0;
+            angleSpeed = 90f; // degrees per second
+
+            mouseHeld = false;
+            mouseReleased = false;
+
+            renderWindow.Closed += (obj, e) => { renderWindow.Close(); };
+            renderWindow.MouseButtonPressed += (_, e) => {
                 if (e.Button == Mouse.Button.Left) mouseHeld = true;
-                Vector2i mousePos = Mouse.GetPosition(window);
+                Vector2i mousePos = Mouse.GetPosition(renderWindow);
                 txtInput.CheckFocus(mousePos);
             };
 
-            window.MouseButtonReleased += (_, e) => {
-                if (e.Button == Mouse.Button.Left) { mouseHeld = false; mouseReleased = true; } 
+            renderWindow.MouseButtonReleased += (_, e) => {
+                if (e.Button == Mouse.Button.Left) { mouseHeld = false; mouseReleased = true; }
             };
-            
-            window.TextEntered += (sender, e) =>
+
+            renderWindow.TextEntered += (sender, e) =>
             {
                 txtInput.HandleEvent(e);
             };
+        }
 
-            while (window.IsOpen)
+        public void Run()
+        {
+            while (this.renderWindow.IsOpen)
             {
-                window.DispatchEvents();
+                Upate();
+                Draw();
+            }
+        }
 
-                Vector2i mousePos = Mouse.GetPosition(window);
-                btnClear.Update(mousePos, mouseHeld);
-                btnFetch.Update(mousePos, mouseHeld);
-                btnDequeue.Update(mousePos, mouseHeld);
-                btnEnqueue.Update(mousePos, mouseHeld);
+        private void UpdateClearButton()
+        {
+            Vector2i mousePos = Mouse.GetPosition(renderWindow);
+            btnClear.Update(mousePos, mouseHeld);
 
-                if (btnClear.IsClicked(mousePos, mouseReleased))
+            if (btnClear.IsClicked(mousePos, mouseReleased))
+            {
+                queue.Clear();
+                errInput.SetText("Queue has been cleared.");
+            }
+        }
+
+        private void UpdateFetchButton()
+        {
+            Vector2i mousePos = Mouse.GetPosition(renderWindow);
+            btnFetch.Update(mousePos, mouseHeld);
+            if (btnFetch.IsClicked(mousePos, mouseReleased))
+            {
+                int? value = queue.Fetch();
+                if (value.HasValue)
                 {
-                    queue.Clear();
-                    errInput.SetText("Queue has been cleared.");
+                    errInput.SetText("The top node is " + value.Value.ToString());
                 }
-                else if (btnFetch.IsClicked(mousePos, mouseReleased))
+                else
+                {
+                    errInput.SetText("Queue is empty.");
+                }
+            }
+        }
+
+        private void UpdateEnqueueButton()
+        {
+            Vector2i mousePos = Mouse.GetPosition(renderWindow);
+            btnEnqueue.Update(mousePos, mouseHeld);
+            if (btnEnqueue.IsClicked(mousePos, mouseReleased))
+            {
+                if (int.TryParse(txtInput.GetText(), out int value))
+                {
+                    queue.Enqueue(value);
+                    txtInput.SetText("");
+                    errInput.SetText("");
+                }
+                else
+                {
+                    errInput.SetText("Invalid input.");
+                }
+            }
+        }
+
+        private void UpdateDequeueButton()
+        {
+            Vector2i mousePos = Mouse.GetPosition(renderWindow);
+            btnDequeue.Update(mousePos, mouseHeld);
+            if (btnDequeue.IsClicked(mousePos, mouseReleased))
+            {
+                if (queue.Empty())
+                {
+                    errInput.SetText("Queue is empty.");
+                }
+                else
                 {
                     int? value = queue.Fetch();
                     if (value.HasValue)
                     {
-                        errInput.SetText("The top node is " + value.Value.ToString());
+                        errInput.SetText("Node(" + value.Value.ToString() + ") has been popped.");
                     }
-                    else
-                    {
-                        errInput.SetText("Queue is empty.");
-                    }
+                    queue.Dequeue();
                 }
-                else if (btnDequeue.IsClicked(mousePos, mouseReleased))
-                {
-                    if (queue.Empty())
-                    {
-                        errInput.SetText("Queue is empty.");
-                    }
-                    else
-                    {
-                        int? value = queue.Fetch();
-                        if (value.HasValue)
-                        {
-                            errInput.SetText("Node(" + value.Value.ToString() + ") has been popped.");
-                        }
-                        queue.Dequeue();
-                    }
-                }
-                else if (btnEnqueue.IsClicked(mousePos, mouseReleased))
-                {
-                    if (int.TryParse(txtInput.GetText(), out int value))
-                    {
-                        queue.Enqueue(value);
-                        txtInput.SetText("");
-                        errInput.SetText("");
-                    }
-                    else
-                    {
-                        errInput.SetText("Invalid input.");
-                    }
-                }
-                mouseReleased = false;
+            }
+        }
 
-                window.Clear(new Color(0, 50, 50));
-                btnClear.Draw(window);
-                btnEnqueue.Draw(window);
-                btnDequeue.Draw(window);
-                btnFetch.Draw(window);
-                txtInput.Draw(window);
-                errInput.Draw(window);
-                queue.Draw(window);
-                window.Display();
+        public void Upate()
+        {
+            renderWindow.DispatchEvents();
+
+            UpdateClearButton();
+            UpdateFetchButton();
+            UpdateEnqueueButton();
+            UpdateDequeueButton();
+
+            mouseReleased = false;
+
+            renderWindow.Clear(new Color(0, 50, 50));
+        }
+
+        private void DrawArrow()
+        {
+            if (queue.Empty()) return;
+
+            Vector2f headPos = queue.GetHeadPos();
+            //Console.WriteLine(headPos);
+
+            ConvexShape arrow = new ConvexShape(3);
+            arrow.SetPoint(0, new Vector2f(headPos.X - 20, headPos.Y + 170));
+            arrow.SetPoint(1, new Vector2f(headPos.X + 20, headPos.Y + 170));
+            arrow.SetPoint(2, new Vector2f(headPos.X, headPos.Y + 140));
+            arrow.FillColor = Color.Red;
+            renderWindow.Draw(arrow);
+
+            headLabel.Position = new Vector2f(headPos.X, headPos.Y + 230);
+
+            if(roate)
+            {
+                // 设置旋转中心为文本中心
+                var bounds = headLabel.GetLocalBounds();
+                headLabel.Origin = new SFML.System.Vector2f(bounds.Left + bounds.Width / 2f, bounds.Top + bounds.Height / 2f);
+
+                // 设置位置和旋转
+                float delta = clock.Restart().AsSeconds();
+                angle += angleSpeed * delta;
+                headLabel.Rotation = angle;
             }
 
+            renderWindow.Draw(headLabel);
+        }
+        public void Draw()
+        {
+            btnClear.Draw(renderWindow);
+            btnEnqueue.Draw(renderWindow);
+            btnDequeue.Draw(renderWindow);
+            btnFetch.Draw(renderWindow);
+
+            txtInput.Draw(renderWindow);
+            errInput.Draw(renderWindow);
+
+            queue.Draw(renderWindow);
+
+            DrawArrow();
+
+            renderWindow.Display();
+        }
+
+        private void Window_KeyPressed(object? sender, KeyEventArgs e)
+        {
+            Window? window = (Window?)sender;
+            if (window != null)
+            {
+                if (e.Code == Keyboard.Key.Escape)
+                {
+                    window.Close();
+                }
+
+                if (e.Code == Keyboard.Key.Space)
+                {
+                    roate = false;
+                    queue.SetColor(Color.Yellow);
+                }
+            }
+        }
+
+        private void Window_KeyReleased(object? sender, KeyEventArgs e)
+        {
+            Window? window = (Window?)sender;
+            if (e.Code == Keyboard.Key.Space)
+            {
+                roate = true;
+                queue.SetColor(defaultColor);
+            }
+        }
+
+        private void Window_Closed(object? sender, EventArgs e)
+        {
+            Window? window = (Window?)sender;
+            if(window != null)window.Close();
         }
     }
 }
--- SFML app/QueueVisualizer.cs
+++ SFML app/QueueVisualizer.cs
@@ -10,16 +10,23 @@ namespace SFML_app
 {
     internal class QueueVisualizer
     {
-        private Queue<int> queue = new Queue<int>();
+        private Queue<int> queue;
         private Font font;
         private Vector2f startPosition;
-        private float radius = 100f;
-        private float spacing = 250f;
+        private float radius;
+        private float spacing;
+        private SFML.Graphics.Color fillColor;
+        private Vector2f headPos;
 
         public QueueVisualizer(Font font, Vector2f startPosition)
         {
+            queue = new Queue<int>();
             this.font = font;
             this.startPosition = startPosition;
+            fillColor = new SFML.Graphics.Color(200, 230, 255);
+            radius = 100f;
+            spacing = 250f;
+            headPos = new Vector2f();
         }
 
         public void Clear()
@@ -48,17 +55,31 @@ namespace SFML_app
                 queue.Dequeue();
         }
 
+        public void SetColor(SFML.Graphics.Color color)
+        {
+            fillColor = color;
+        }
+
+        public Vector2f GetHeadPos()
+        {
+            return headPos;
+        }
+
         public void Draw(RenderWindow window)
         {
+            headPos.X = -1; // Reset head position
+            
             float x = startPosition.X;
             float y = startPosition.Y;
-            int index = 0;
 
-            foreach (int value in queue)
+            List<int> tempList = queue.ToList();
+            for (int i = tempList.Count - 1; i >= 0; i--)
             {
+                int value = tempList[i];
+                
                 CircleShape circle = new CircleShape(radius)
                 {
-                    FillColor = new Color(200, 230, 255),
+                    FillColor = fillColor,
                     //OutlineColor = Color.Black,
                     //OutlineThickness = 2,
                     Position = new Vector2f(x, y)
@@ -66,7 +87,7 @@ namespace SFML_app
 
                 Text label = new Text(value.ToString(), font, 50)
                 {
-                    FillColor = Color.Black,
+                    FillColor = SFML.Graphics.Color.Black,
                 };
 
                 // 获取文字边界
@@ -81,8 +102,13 @@ namespace SFML_app
                 window.Draw(circle);
                 window.Draw(label);
 
+                if (i == 0) // Head
+                {
+                    headPos.X = circle.Position.X + radius;
+                    headPos.Y = circle.Position.Y + radius;
+                }
+
                 x += spacing;
-                index++;
             }
         }
     }
